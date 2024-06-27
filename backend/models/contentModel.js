d01e492fb72570a901eeb676a0444ea624dc28f9
Kim-Tsok <appkim02@gmail.com>
2024-06-27 07:04:05 +0100

    Updated backend

---
+++ backend/models/contentModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const contentSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    about: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("content", contentSchema);
--- backend/routes/content.js
+++ backend/routes/content.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Content = require("../models/contentModel");
 
 const router = express.Router();
 const mongoose = require("mongoose");
@@ -13,7 +14,15 @@ router.get("/:id", (res, req) => {
 });
 
 // POST a new workout
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
+  const { title, about } = req.body;
+
+  try {
+    const content = await Content.create({ title, about });
+    res.status(200).json(content);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
   res.json({ mssg: "POST a new book" });
 });
 
