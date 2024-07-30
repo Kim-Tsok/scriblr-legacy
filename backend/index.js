decd0a7a519b97713369b8e7a3c7225c712933f8
Kim-Tsok <appkim02@gmail.com>
2024-07-30 17:20:31 +0100

    bug fixes

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,6 @@ const cors = require("cors");
 const app = express();
 
 // middleware
-app.use(express.json({ limit: "50mb" }));
-app.use(fileUpload({ useTempFiles: true }));
-app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 
 app.use(
   cors({
