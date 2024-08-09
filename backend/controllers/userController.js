88eac8db517d77f88f222226d5a1e1d1fee6545f
Kim-Tsok <appkim02@gmail.com>
2024-08-09 23:58:37 +0100

    just more tears

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,13 +6,20 @@ const createToken = (_id) => {
   return jwt.sign({ _id: _id }, process.env.SECRET, { expiresIn: "7d" });
 };
 
-// login user
 const loginUser = async (req, res) => {
   const { username, password } = req.body;
 
   try {
+    console.log(`Attempting login for user: ${username}`);
     const user = await User.login(username, password);
-    const token = createToken(user?._id);
+    console.log(`User retrieved:`, user);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const token = createToken(user._id);
+    console.log(`Token created:`, token);
 
     res.status(200).json({
       username,
@@ -36,7 +43,7 @@ const signupUser = async (req, res) => {
       email,
       password
     );
-    const token = createToken(user?._id);
+    const token = createToken(user._id);
 
     res.status(201).json({
       username,
