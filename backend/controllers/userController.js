a3f57b70ecae6a9398e77c8109ddaa7275ebe206
Kim-Tsok <appkim02@gmail.com>
2024-08-09 23:54:24 +0100

    just tears

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const createToken = (_id) => {
-  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "7d" });
+  return jwt.sign({ _id: _id }, process.env.SECRET, { expiresIn: "7d" });
 };
 
 // login user
