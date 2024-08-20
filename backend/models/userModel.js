cdb32468d6ee13c46ae30ffe2e512b75987024d7
Kim-Tsok <appkim02@gmail.com>
2024-08-20 18:46:44 +0100

    fixed signup

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -51,9 +51,6 @@ userSchema.statics.signup = async function (
   if (!isEmail(email)) {
     throw Error("Email is not valid");
   }
-  if (isStrongPassword(password)) {
-    throw Error("Password not strong enough");
-  }
   const exists = await this.findOne({ email });
 
   if (exists) {
