3d22c52bbc97bc717d256b47ef0b5e96f7fb9e1b
Kim-Tsok <appkim02@gmail.com>
2024-09-05 23:04:07 +0100

    Started working on a book select form, about to abandon project

---
+++ frontend/src/components/Create Menu/Books/SelectBookOption.jsx
@@ -0,0 +1,14 @@
+const SelectBookOption = () => {
+  return (
+    <>
+      <div className="w-screen flex items-center justify-center flex-col">
+        <h1>Select an option.</h1>
+        <div className="w-[30vw] h-20">
+          <h1>Submit pdf</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SelectBookOption;
